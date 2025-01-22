4d0eb337af0be7e28e8b50e87ba777620d82fd51
Babu labana <115482429+babulabana@users.noreply.github.com>
2025-01-22 21:55:13 +0530

    hover

--- src/App.jsx
+++ src/App.jsx
@@ -22,29 +22,28 @@ function App() {
     <>
    <div className=' bg-gray-500 flex justify-center items-center min-h-screen'>
     <form action=" " className=' h-[490px] w-[380px] rounded-xl p-5 bg-black  '> 
-      <input type="text"  readOnly value={input} className='focus:outline-none w-full h-[45px]   bg-gray-200 ' />
+      <input type="text"  readOnly value={input} className='focus:outline-none w-full h-[50px] p-2 rounded-xl text-white text-3xl  bg-gray-400 ' />
       <div className=' text-white grid grid-cols-4 grid-rows-5 text-3xl gap-y-2 my-3 items-center justify-center'>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={clear}>c</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("+/-")}>+/-</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("%")}>%</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("/")}>/</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("7")}>7</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("8")}>8</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("9")}>9</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("*")}>x</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("4")}>4</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("5")}>5</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("6")}>6</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("-")}>-</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("1")}>1</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("2")}>2</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("3")}>3</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("+")}>+</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("0")}>0</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display("00")}>00</span>
-
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={()=>display(".")}>.</span>
-        <span className='h-[70px] w-[70px]  rounded-3xl  text-black bg-white flex justify-center items-center cursor-pointer' onClick={calculator}>=</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl   text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={clear}>c</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("+/-")}>+/-</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("%")}>%</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("/")}>/</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("7")}>7</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("8")}>8</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("9")}>9</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("*")}>x</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("4")}>4</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("5")}>5</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("6")}>6</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("-")}>-</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("1")}>1</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("2")}>2</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("3")}>3</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("+")}>+</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("0")}>0</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700  hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display("00")}>00</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-400 hover:bg-gray-700 hover:border-4 flex justify-center items-center cursor-pointer' onClick={()=>display(".")}>.</span>
+        <span className='h-[70px] w-[70px]  rounded-3xl  text-white bg-gray-700 hover:bg-gray-400  hover:border-4 flex justify-center items-center cursor-pointer' onClick={calculator}>=</span>
 
       </div>
     </form>
